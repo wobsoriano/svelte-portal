c0b253f73da9ff002495b2868533aedec3ea51c0
wobsoriano <sorianorobertc@gmail.com>
2024-10-15 16:31:37 -0700

    test: add dynamic target test

--- tests/Portal.test.ts
+++ tests/Portal.test.ts
@@ -1,6 +1,7 @@
 import { describe, it, beforeEach, expect, afterEach } from 'vitest';
 import TestPortalSelector from './TestPortalSelector.svelte';
 import TestPortalElement from './TestPortalElement.svelte';
+import TestPortalDynamic from './TestPortalDynamic.svelte';
 import { mount, flushSync, unmount } from 'svelte';
 
 describe('<Portal />', () => {
@@ -28,12 +29,32 @@ describe('<Portal />', () => {
 		unmount(app);
 	});
 
-	it('should be rendered in a specific HTML element using element', () => {
+	it('should be rendered in a specific HTML element using actual node', () => {
 		const app = mount(TestPortalElement, { target });
 
 		flushSync();
 
 		const renderedInTargetElement = target.querySelector('#renderedInTargetElement');
+
+		expect(renderedInTargetElement).not.toBe(null);
+
+		unmount(app);
+	});
+
+	it('should be rendered in a dynamic target', async () => {
+		const app = mount(TestPortalDynamic, { target: document.body });
+
+		flushSync();
+
+		let renderedInTargetElement = target.querySelector('#renderedInTargetElement');
+
+		expect(renderedInTargetElement).toBe(null);
+
+		document.body.querySelector('button')?.click();
+
+		flushSync();
+
+		renderedInTargetElement = target.querySelector('#renderedInTargetElement');
 
 		expect(renderedInTargetElement).not.toBe(null);
 
---
+++ tests/TestPortalDynamic.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Portal from '../src/lib/Portal.svelte';
+
+	let target = $state<string>('body');
+</script>
+
+<button onclick={() => (target = '#target')}>Replace target</button>
+
+<Portal {target}>
+	<div id="renderedInTargetElement"></div>
+</Portal>
