d71e49059350e33df36dec794da87369a021ae81
wobsoriano <sorianorobertc@gmail.com>
2024-10-15 16:19:12 -0700

    chore: simplify vite test config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
 	test: {
 		include: ['{src,tests}/**/*.{test,spec}.{js,ts}'],
 		environment: 'jsdom'
 	},
 	// Fix to issue https://github.com/sveltejs/svelte/discussions/12037
-	resolve: {
-		conditions: mode === 'test' ? ['browser'] : []
-	},
+	resolve: process.env.VITEST
+		? {
+				conditions: ['browser']
+			}
+		: undefined,
 	plugins: [sveltekit()]
-}));
+});
